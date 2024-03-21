8d38f01392cf2a1de8ffb3da557e7bee7c679b4f
DanielWebMap <Daniel.Schoenung@student.uibk.ac.at>
2024-03-21 17:49:29 +0100

    Änderung

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([63.5321038, -19.511292], 15);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+var map = L.map('map').setView([-34.426769, 172.67751], 15);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 
-        var marker = L.circleMarker([63.5321038, -19.511292], {color: '#252850', fillColor: '#38caff', fillOpacity: '0.6'}).addTo(map);
-        marker.bindPopup("<b>Skógafoss</b> <br> GPS: 63.532104 -19.511292 ").openPopup();
+var marker = L.circleMarker([-34.426769, 172.67751], { color: '#252850', fillColor: '#38caff', fillOpacity: '0.6' }).addTo(map);
+marker.bindPopup("<b>Cape Reinga</b><br>Nördlichster Punkt von Neuseeland <br> GPS: -34.426769 172.67751").openPopup();
