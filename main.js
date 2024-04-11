ce6cb916d80beb677d165c70786dfd2ff73ccd58
DanielWebMap <Daniel.Schoenung@student.uibk.ac.at>
2024-04-11 16:07:09 +0200

    Definition von Variablen für Koordinaten, Zoom

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-let map = L.map('map').setView([-34.426769, 172.67751], 15);
+let lat = -34.426769;
+let lng = 172.67751;
+let zoom = 15;
+
+let map = L.map('map').setView([lat, lng], zoom);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
 
 let marker = L.circleMarker([-34.426769, 172.67751], { color: '#252850', fillColor: '#fef5d4', fillOpacity: '0.6' }).addTo(map);
 marker.bindPopup("<b>Cape Reinga</b><br>Nördlichster Punkt von Neuseeland <br> GPS: -34.426769 172.67751").openPopup();
